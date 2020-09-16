80b9ea0ed657244b908d937956aa126a47db2617
Muskan Bartaula <muskan.bartaula109@gmail.com>
2020-09-16 19:59:16 +0545

    Only one instance of monthly saving can be created.

--- src/members/models.py
+++ src/members/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class MonthlySaving(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
@@ -7,6 +8,21 @@ class MonthlySaving(models.Model):
 
     def __str__(self):
         return str(self.amount)
+    
+    def clean(self, *args, **kwargs):
+        if not self.pk and MonthlySaving.objects.exists():
+            raise ValidationError(
+                "MonthlySaving instance already exists.",
+                code='invalid'
+            )
+        super().clean(*args, **kwargs)
+
+    def full_clean(self, *args, **kwargs):
+        return self.clean(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
 
 
 class Member(models.Model):
