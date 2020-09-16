4d53e80c568d287aa60f73d11a5b17c678e19cbd
Muskan Bartaula <muskan.bartaula109@gmail.com>
2020-09-16 12:04:59 +0545

    Deleted unwanted line space

--- src/transactions/templatetags/nepali_date.py
+++ src/transactions/templatetags/nepali_date.py
@@ -4,11 +4,9 @@ from nepali_date import NepaliDate
 
 register = template.Library()
 
-
 def nepali_date(value, arg):
     nepali_date = NepaliDate.to_nepali_date(value)
     formatted_nepali_date = nepali_date.strfdate(arg)
     return formatted_nepali_date
 
 register.filter('nepali_date', nepali_date)
-
