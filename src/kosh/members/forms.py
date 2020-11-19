5ddb1f6a5aec983e27678028e787cd4fe993ea9f
Muskan Bartaula <muskan.bartaula109@gmail.com>
2020-11-19 21:54:04 +0545

    Previous Due added for first time when member is added

--- src/kosh/members/forms.py
+++ src/kosh/members/forms.py
@@ -11,21 +11,27 @@ from crispy_forms.layout import (
 from .models import Member
 
 class MemberForm(forms.ModelForm):
+    previous_due = forms.DecimalField(decimal_places=2,
+                                      max_digits=20, required=False)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['membership_id'].widget = forms.TextInput()
         self.fields['number_of_share'].widget = forms.TextInput()
+        self.fields['previous_due'].widget = forms.TextInput()
         self.helper = FormHelper()
         self.helper.form_method = "post"
         self.helper.layout = Layout(
             Field('membership_id', autocomplete="off"),
             Field('name', autocomplete="off"),
             Field('number_of_share', autocomplete="off"),
+            Field('previous_due', autocomplete="off"),
 
             ButtonHolder(
                 Submit('submit', 'Submit', css_class="btn btn-success btn-block")
             )
         )
+
 
     class Meta:
         model = Member
--- src/kosh/members/views.py
+++ src/kosh/members/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from django.urls import reverse
 
+from kosh.loans.models import Loan
 from .forms import MemberForm
 from .models import Member
 
@@ -28,6 +29,13 @@ class MemberCreateView(LoginSuccessMessageMixin, MemberMixin, generic.CreateView
     form_class = MemberForm
     success_message = 'Member "%(name)s" successfully created.'
 
+    def form_valid(self, form):
+        previous_due = form.cleaned_data.get('previous_due')
+        member_id = form.cleaned_data.get('membership_id')
+        self.object = form.save()
+        loan_obj = Loan.objects.create(member_id=self.object.id, amount=previous_due)
+        return redirect('members:index')
+
 
 class MemberListView(generic.ListView):
     model = Member
@@ -38,6 +46,20 @@ class MemberUpdateView(LoginSuccessMessageMixin, MemberMixin, generic.UpdateView
     template_name = "members/member_form.html"
     form_class = MemberForm
     success_message = 'Member "%(name)s" successfully updated.'
+
+    def get_initial(self):
+        member_loan_qs = Loan.objects.filter(member_id=self.object.id)
+        previous_due = 0.00
+        if member_loan_qs.exists():
+            member_loan_obj = member_loan_qs.first()
+            previous_due = member_loan_obj.amount
+        initial_data = {'previous_due': previous_due}
+        return initial_data
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['member_update'] = True
+        return context
 
 
 class MemberDeleteView(LoginRequiredMixin, MemberMixin, generic.DeleteView):
