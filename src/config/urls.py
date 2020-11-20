b89bdbdc36f27481d757f4e0be2fc6d0a2263cf4
Muskan Bartaula <muskan.bartaula109@gmail.com>
2020-11-20 13:46:35 +0545

    individual members transaction can be exported to excel

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,15 +3,16 @@ from django.contrib import admin
 from django.urls import path, include
 
 from kosh.core.views import (
-    transactions_filter_page,
     individual_member_monthly_transaction,
-    members_monthly_transaction, export_to_excel
+    members_monthly_transaction, export_to_excel,
+    individual_transaction_to_excel
 )
 
 urlpatterns = [
     path('', members_monthly_transaction, name="members_monthly_transaction"),
     path('individual/', individual_member_monthly_transaction, name="individual_member_monthly_transaction"),
     path('export/<int:year>/<int:month>/', export_to_excel, name="export_to_excel"),
+    path('individual-member-transaction-to-excel/<int:member_id>/<str:start_date>/<str:end_date>', individual_transaction_to_excel, name="individual_transaction_to_excel"),
     path('admin/', admin.site.urls),
     path('accounts/', include('kosh.accounts.urls')),
     path('members/', include('kosh.members.urls')),
--- src/kosh/core/views.py
+++ src/kosh/core/views.py
@@ -1,10 +1,12 @@
 import datetime
 import xlwt
+from datetime import datetime
+
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 
 from nepali_date import NepaliDate
@@ -16,17 +18,16 @@ from kosh.transactions.forms import (TransactionDateFilterForm,
 from kosh.transactions.models import Transaction
 
 @login_required
-def transactions_filter_page(request):
-    context = {}
-    return render(request, "transactions/index.html", context)
-
-@login_required
 def individual_member_monthly_transaction(request):
     member_id = request.GET.get('member_id')
     from_date = request.GET.get('from_date')
     to_date = request.GET.get('to_date')
 
     form = TransactionDateRangeFilterForm()
+
+    from_date_in_ad = datetime.today().date()
+    to_date_in_ad = datetime.today().date()
+
 
     member = None
     transactions = Transaction.objects.none()
@@ -57,6 +58,8 @@ def individual_member_monthly_transaction(request):
     context = {
         'form': form,
         'member': member,
+        'start_date': from_date_in_ad.strftime('%Y-%m-%d'),
+        'end_date': to_date_in_ad.strftime('%Y-%m-%d'),
         'transactions': transactions,
     }
     return render(request, "transactions/individual_member_monthly_transaction.html", context)
@@ -90,6 +93,7 @@ def members_monthly_transaction(request):
     }
 
     return render(request, "transactions/members_monthly_transaction.html", context)
+
 
 @login_required
 def export_to_excel(request, year, month):
@@ -146,6 +150,84 @@ def export_to_excel(request, year, month):
                 ws.write(row_num, 10, transaction.remaining_loan_amount, font_style)
                 ws.write(row_num, 11, transaction.additional_loan_amount, font_style)
                 ws.write(row_num, 12, transaction.total_loan_amount, font_style)
+
+    wb.save(response)
+    return response
+
+
+
+@login_required
+def individual_transaction_to_excel(request, member_id, start_date, end_date):
+    response = HttpResponse(content_type="application/ms-excel")
+    response['Content-Disposition'] = 'attachment; filename="kosh.xls"'
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Individual_Transactions')
+
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    member = get_object_or_404(Member, pk=member_id)
+
+    ws.write(row_num, 0, "Member's Name", font_style)
+    ws.write(row_num, 1, member.name, font_style)
+
+    row_num += 1
+    ws.write(row_num, 0, 'Number of Share', font_style)
+    ws.write(row_num, 1, member.number_of_share, font_style)
+
+    row_num += 1
+    ws.write(row_num, 0, 'Total Saving', font_style)
+    ws.write(row_num, 1, member.membersaving.amount, font_style)
+
+    row_num += 1
+    ws.write(row_num, 0, 'Total Loan Amount', font_style)
+    ws.write(row_num, 1, member.loan.amount, font_style)
+
+    row_num += 1
+    row_num += 1
+
+    columns = [
+        'Date',
+        'Previous Month Loan',
+        'Monthly Saving',
+        'Loan Amount Paid',
+        'Interest',
+        'Fine',
+        'Others',
+        'Total Amount Paid',
+        'Remaining Loan Amount',
+        'Additional Loan Amount',
+        'Total Loan Amount',
+    ]
+
+    for col_num, column in enumerate(columns):
+        ws.write(row_num, col_num, column, font_style)
+
+    font_style = xlwt.XFStyle()
+
+    from_date = datetime.strptime(start_date, '%Y-%m-%d')
+    to_date = datetime.strptime(end_date, '%Y-%m-%d')
+    transactions = Transaction.objects.filter(member=member, date__range=(from_date, to_date))
+    members = Member.objects.all().order_by('membership_id')
+
+    for transaction in transactions:
+        transaction_nepali_date = NepaliDate.to_nepali_date(transaction.date)
+        transaction_strdate_in_bs = transaction_nepali_date.strfdate('%Y-%m-%d') 
+        row_num += 1
+        ws.write(row_num, 0, transaction_strdate_in_bs, font_style)
+        ws.write(row_num, 1, transaction.previous_month_loan, font_style)
+        ws.write(row_num, 2, transaction.monthly_saving, font_style)
+        ws.write(row_num, 3, transaction.loan_amount_paid, font_style)
+        ws.write(row_num, 4, transaction.interest, font_style)
+        ws.write(row_num, 5, transaction.fine, font_style)
+        ws.write(row_num, 6, transaction.others, font_style)
+        ws.write(row_num, 7, transaction.total_amount_paid, font_style)
+        ws.write(row_num, 8, transaction.remaining_loan_amount, font_style)
+        ws.write(row_num, 9, transaction.additional_loan_amount, font_style)
+        ws.write(row_num, 10, transaction.total_loan_amount, font_style)
 
     wb.save(response)
     return response
